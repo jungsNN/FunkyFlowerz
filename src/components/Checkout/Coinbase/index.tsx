d9a81211e7336f823f7a7cd3427d9809306060c9
suzukidogemom <suzukidogemom@suzi.link>
2022-05-28 22:53:28 -0400

    add beta checkout

--- src/components/Checkout/Coinbase/index.tsx
+++ src/components/Checkout/Coinbase/index.tsx
@@ -3,7 +3,7 @@ const CoinbaseCheckout = () => {
   return (
     <div>
       <a className="donate-with-crypto"
-        href="https://commerce.coinbase.com/checkout/5bf785aa-e56d-4570-85d0-ffb9e62c52e6"
+        href={`https://commerce.coinbase.com/checkout/${process.env.NEXT_COINBASE_CHECKOUT_ID}`}
         target="_blank"
         rel="noreferrer"
         style={{color: '#e4ebff'}}
