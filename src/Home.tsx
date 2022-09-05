5a1b647e37ff4873324407b706eea4d08c1ea87d
Jenny Jung <jjung9310@gmail.com>
2022-09-04 21:39:19 -0400

    updated splash page

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,8 @@
 // import styled from "styled-components";
 import { Container, Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 import AppBar, { AppBarProps } from "./AppBar";
 import FunkyFlowerzTitle from "./components/svgs/FunkyFlowerzTitle";
 
@@ -18,6 +20,19 @@ const FUNKY_FLOWERZ_QUOTE = "There are always flowers for those who want to see 
 const FUNKY_FLOWERS_DESC = "A collection of 2700 hand-made flowers from 9 species, including dozens of insect variants and mutations. 50% proceeds donated to environmental organizations."
 
 const Home = (props: AppBarProps) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window && typeof window !== 'undefined') {
+        window.addEventListener('resize', () => {
+            setIsMobile(window.innerWidth < 1024)
+        })
+
+        return () => {
+            window.removeEventListener('resize', () => {})
+        }
+    }
+  }, [])
   // const [isUserMinting, setIsUserMinting] = useState(false);
   // const [candyMachine, setCandyMachine] = useState<CandyMachineAccount>();
   // const [alertState, setAlertState] = useState<AlertState>({
@@ -463,33 +478,80 @@ const Home = (props: AppBarProps) => {
       <Container style={{ position: "relative", marginTop: 100 }}>
         <Grid
           container
-          direction="row"
+          direction={isMobile ? "column" : "row"}
           justifyContent="space-between"
           wrap="nowrap"
+          style={{gridGap: '35px'}}
         >
           <Grid
             container
             direction="column"
-            style={{gridGap: '35px'}}
+            justifyContent="space-between"
+            style={{gridGap: '35px', height: '100%'}}
           >
-            <div className="funky-flowerz-title">
+            <SplashTitle>
               <FunkyFlowerzTitle />
-            </div>
-            <div className="funky-flowerz-quote">
+            </SplashTitle>
+            <SplashQuote>
               <Typography>{FUNKY_FLOWERZ_QUOTE}</Typography>
-            </div>
-            <Grid>
-              <Typography style={{ fontWeight: "bold" }}>Collective Digital Art</Typography>
+            </SplashQuote>
+            <FunkyFlowerzDescription>
+              <Typography style={{fontWeight: 700, marginBottom: 10}}>Collective Digital Art</Typography>
               <Typography>{FUNKY_FLOWERS_DESC}</Typography>
-            </Grid>
+            </FunkyFlowerzDescription>
           </Grid>
-          <div>
+          <SplashImage>
             <img src="./funkyflowerz-top.png" alt="funkyflowerz top" />
-          </div>
+          </SplashImage>
         </Grid>
+        <Samples>
+          <img src="./funkyflowerz-bg.png" alt="funky flowerz samples" />
+        </Samples>
       </Container>
     </Container>
   );
 };
+
+const SplashTitle = styled.div`
+  @media(max-width: 1023px) {
+    svg {
+      width: 100%;
+      height: 100%;
+      max-height: 167px;
+    }
+  }
+`;
+
+const SplashQuote = styled.div`
+  p {
+    font-size: 40px;
+    font-height: 48px;
+  }
+`;
+
+const FunkyFlowerzDescription = styled.div`
+  margin-top: 66px;
+  font-size: 24px;
+  line-height: 25px;
+`;
+
+const SplashImage = styled.div`
+
+  @media(max-width: 1023px) {
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
+
+const Samples = styled(Container)`
+  padding-top: 140px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export default Home;
