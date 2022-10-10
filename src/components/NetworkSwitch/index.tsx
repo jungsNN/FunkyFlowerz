d2b5d489e5a652640c77317d590cbed3a7716908
Jenny Jung <jjung9310@gmail.com>
2022-10-09 21:41:14 -0400

    ran yarn lint

--- src/components/NetworkSwitch/index.tsx
+++ src/components/NetworkSwitch/index.tsx
@@ -6,55 +6,55 @@ import styled from "styled-components";
 import colors from "../../theme/colors";
 
 const ColoredSwitch = styled(Switch)(() => ({
-    "& .MuiSwitch-switchBase.Mui-checked": {
-      color: colors.purple,
-      "&:hover": {
-        backgroundColor: alpha(colors.purple, 0.3),
-      },
+  "& .MuiSwitch-switchBase.Mui-checked": {
+    color: colors.purple,
+    "&:hover": {
+      backgroundColor: alpha(colors.purple, 0.3),
     },
-    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
-      backgroundColor: colors.purple,
+  },
+  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+    backgroundColor: colors.purple,
+  },
+  "& .MuiSwitch-switchBase": {
+    color: colors.orange,
+    "&:hover": {
+      backgroundColor: alpha(colors.orange, 0.3),
     },
-    "& .MuiSwitch-switchBase": {
-      color: colors.orange,
-      "&:hover": {
-        backgroundColor: alpha(colors.orange, 0.3),
-      },
-    },
-    "& .MuiSwitch-switchBase + .MuiSwitch-track": {
-      backgroundColor: colors.orange,
-    },
-  }));
-  
+  },
+  "& .MuiSwitch-switchBase + .MuiSwitch-track": {
+    backgroundColor: colors.orange,
+  },
+}));
+
 const NetworkSwitch = ({
-    isMobile,
-    network,
-    onToggle,
-  }: {
-    isMobile: boolean;
-    network: string;
-    onToggle: () => void;
-  }) => {
-    const labelProps = {
-      componentsProps: { typography: { fontSize: isMobile ? "10px" : "12px" } },
-    };
-    return (
-      <FormGroup>
-        <FormControlLabel
-          {...labelProps}
-          control={
-            <ColoredSwitch
-              checked={network === "mainnet-beta"}
-              size={isMobile ? "small" : "medium"}
-              onChange={onToggle}
-              value={network}
-            />
-          }
-          label={network}
-          labelPlacement="bottom"
-        />
-      </FormGroup>
-    );
+  isMobile,
+  network,
+  onToggle,
+}: {
+  isMobile: boolean;
+  network: string;
+  onToggle: () => void;
+}) => {
+  const labelProps = {
+    componentsProps: { typography: { fontSize: isMobile ? "10px" : "12px" } },
   };
+  return (
+    <FormGroup>
+      <FormControlLabel
+        {...labelProps}
+        control={
+          <ColoredSwitch
+            checked={network === "mainnet-beta"}
+            size={isMobile ? "small" : "medium"}
+            onChange={onToggle}
+            value={network}
+          />
+        }
+        label={network}
+        labelPlacement="bottom"
+      />
+    </FormGroup>
+  );
+};
 
 export default NetworkSwitch;
