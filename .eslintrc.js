6928279d3bc25844508315907bdda06d4c6c2bd8
Jenny Jung <jenny.jung3@wework.com>
2022-11-26 00:12:58 -0500

    feat: configure lint-staged and pre-commit husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended"
+    "plugin:@typescript-eslint/eslint-recommended",
+    "prettier"
   ],
   globals: {
     Atomics: 'readonly',
---
+++ lint-staged.config.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+module.exports = {
+    // type check typescirpt files
+    './src/**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+  
+    // Lint then format typescript and js files
+    './src/**/*.(ts|tsx|js)': (filenames) => [
+      `yarn eslint --fix ${filenames.join(' ')}`,
+      `yarn prettier --write ${filenames.join(' ')}`,
+    ],
+  
+    // Format MarkDown and JSON
+    '**/*.(md|json)': (filenames) =>
+      `yarn prettier --write ${filenames.join(' ')}`,
+  }
+  
