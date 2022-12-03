50eb813c3f7521443e1d65dd0bda4b55dab686d4
Jenny Jung <jenny.jung3@wework.com>
2022-12-03 17:05:43 -0500

    fix: adjust mobile home view

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,34 @@
 import styled from "styled-components";
-import { Container } from "../../components/shared";
+import { Container, Grid } from "../../components/shared";
+import FlowerSamples from "../../components/FlowerSamples";
 import HomeSplashBG from "../../components/svgs/HomeSplashBG";
 import Page from "../../components/Page";
+import { useStore } from "../../hooks";
 
 const Home = () => {
-  return (
+  const isMobile = useStore.getState().isMobile;
+
+  return isMobile ? (
+    <Page>
+      <SplashContainer>
+        <SplashHeader
+          container
+          align="center"
+          direction="column-reverse"
+          display="flex"
+          gap="48px"
+          gridCols="repeat(2, 1fr)"
+          wrap="nowrap"
+        >
+          <HomeSplashBG isMobile />
+          <SplashImage>
+            <img src="./funky-flowerz-top.gif" alt="funkyflowerz-top" />
+          </SplashImage>
+        </SplashHeader>
+        <FlowerSamples />
+      </SplashContainer>
+    </Page>
+  ) : (
     <Page>
       <SplashContainer>
         <HomeSplashBG />
@@ -19,6 +43,29 @@ const SplashContainer = styled(Container)`
   height: 100%;
 
   svg {
+    width: 100%;
+    height: 100%;
+  }
+
+  ${(props) => props.theme.mediaQueries.mobile} {
+    display: grid;
+    grid-auto-flow: row;
+    align-items: start;
+    justify-content: center;
+    grid-gap: 64px;
+  }
+`;
+
+const SplashHeader = styled(Grid)`
+  height: 100%;
+  justify-content: center;
+`;
+
+const SplashImage = styled.div`
+  width: calc(100vw * (128 / 480));
+  height: calc(100vw * (128 / 480));
+
+  img {
     width: 100%;
     height: 100%;
   }
