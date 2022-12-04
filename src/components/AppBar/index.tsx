4e4a091c5cbbefa2fe71e278917523038aaeca4d
Jenny Jung <jenny.jung3@wework.com>
2022-12-04 04:08:21 -0500

    fix: readjust column sizes

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -731,7 +731,7 @@ const AppBar: React.FC<AppBarProps & ConnectionProps> = (props) => {
                   style={{
                     width: "100%",
                   }}
-                  xs={5}
+                  xs={4}
                 >
                   <Title thin variant="caption">
                     {isWhitelistUser && discountPrice
@@ -761,7 +761,7 @@ const AppBar: React.FC<AppBarProps & ConnectionProps> = (props) => {
                   style={{
                     width: "100%",
                   }}
-                  xs={7}
+                  xs={8}
                 >
                   <Title thin variant="caption">
                     Remaining
